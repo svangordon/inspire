f3491b56bd963e39c1669f8595b9f44d3e5d94b5
fangs073 <stephen.van.gordon@gmail.com>
2016-02-01 23:45:24 -0700

    worked on Quote constructor.  Tomorrow, work on methods, starting w/ getters & setters for ratings; also come up w/ methods for manipulating rankings

--- angular/quote/quoteDisplayController.js
+++ angular/quote/quoteDisplayController.js
@@ -2,4 +2,7 @@ angular.module('app')
 	.controller('quoteDisplayController', ['$scope', 'quoteFactory', function(scope, quotes) {
 		scope.quotes = quotes.quotes;
 		console.log(scope.quotes[0].author.fname)
+
+		scope.obj = quotes.aQuoteObj;
+		console.log(scope.obj)
 	}])
--- angular/quote/quoteFactory.js
+++ angular/quote/quoteFactory.js
@@ -3,7 +3,68 @@
 angular.module('app')
 	.factory('quoteFactory', [function(){
 
+		var Quote = function(author, quote, metadata) {
+			this.author = {
+				fname : author.fname,
+				lname : author.lname,
+				desc : author.desc,
+				id : author.id
+			};
+
+			this.quote = {
+				content : quote.content,
+				context : quote.context
+			};
+
+			this.metadata = {
+				ratingsArray : metadata.ratings,
+				id : metadata.id,
+				get rating() {
+					function roundHalf (num) {
+						return Math.round(num*2)/2;
+					}
+					roundedBool = roundedBool === undefined ? true : false
+					var voteCount = arr.reduce( ( prev , cur ) => prev + cur );
+					var voteScore = arr.reduce( ( prev , cur , i ) => prev + cur * i );
+					var rating = voteScore / voteCount;
+					return roundedBool ? roundHalf(rating) : rating;
+				},
+				
+			};
+		}
+
+		Object.defineProperty(Quote.prototype, 'rating', {
+			get : function ( arr, roundedBool ) {
+					function roundHalf (num) {
+						return Math.round(num*2)/2;
+					}
+					roundedBool = roundedBool === undefined ? true : false
+					var voteCount = arr.reduce( ( prev , cur ) => prev + cur );
+					var voteScore = arr.reduce( ( prev , cur , i ) => prev + cur * i );
+					var rating = voteScore / voteCount;
+					return roundedBool ? roundHalf(rating) : rating;
+				}
+		});
+
+		Object.defineProperty(Quote.prototype, 'votesNumber', {
+			get : function () {
+				return this.metadata.ratingsArray.reduce( ( prev , cur ) => prev + cur );
+			}
+		})
+
+		
+
 		return {
+			aQuoteObj : new Quote({
+						fname : 'Gillian',
+						lname : 'Anderson',
+						desc : 'Actress',
+						id : 'a100001'
+					}, {content : 'Buckets are good for many a thing. Make sure you empty out a dirty bucket first, if the bucket is dirty.',
+						context : 'Unprompted.'},
+						{ratings : [10,20,30,40,50], id : 'a100001'})
+			,
+
 			quotes: [
 				{
 					author : {
@@ -12,6 +73,7 @@ angular.module('app')
 						desc : 'Actress',
 						id : 'a100001'
 					},
+					
 					content : "Buckets are good for many a thing. Make sure you empty out a dirty bucket first, if the bucket is dirty.",
 					context : 'Unprompted.',
 					ratings : [10,20,30,40,50],
